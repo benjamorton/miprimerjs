67c9e2b231ec3b9070326e05acebad1633015987
Benjamin Morton <benjamorton98@gmail.com>
2021-06-01 21:38:19 -0300

    lista diferencia de edad 2.0

---
+++ js/diferenciaDeEdad.js
@@ -0,0 +1,21 @@
+// algoritmo:
+// crear variables y obtener su valor
+// preguntarme cual de los dos datos es mayor
+// hacer la diferencia en el orden correspondiente
+// devolver una respuesta
+
+let edad = parseInt(prompt("cual es tu edad?");
+let edad = parseInt(prompt("cual es la edad de tu hno?");
+
+if (edad > edad2)
+{
+    let diferencia = edad - edad2;
+    alert("tu hermano y tu se llevan "+diferencia+" años, tu eres el mayor");
+}
+else if (edad < edad2)
+{
+    let diferencia = edad2 - edad;
+    alert("tu hermano y tu se llevan "+diferencia+" años, tu eres el menor");
+}
+
+alert("tu hno y tu se llevan "+ diferencia+" años");
---
+++ js/holamundo.js
@@ -0,0 +1,25 @@
+// variables de texto o string
+let nombre = "Benja";
+let nombre2 = "Pedro";
+console.log(nombre);
+// alert("hola mundo!");
+// tres formas de enviar!mostrar info al navegador
+
+document.write("<h1> hola mundo!</h1>");
+nombre = "juana";
+console.log(nombre);
+
+// pedir datos al usuario
+
+// let nombreUsuario = prompt("como es tu nombre");
+// let nombreUsuario2 = prompt("como se llama tu compañero");
+
+// esto se llama concatenar
+// document.write("hola "+nombreUsuario+" y "+nombreUsuario2);
+
+// variables numericas
+let edad = prompt("cual es tu edad?");
+let edad = prompt("cual es la edad de tu hno?");
+let diferencia = edad - edad2 ;
+alert("tu hno y tu se llevan "+ diferencia+" años");
+
